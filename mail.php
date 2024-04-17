3d8fb98212bb334ebc5b6225c853bf34d672e585
Enescan Akyüz <77545118+EnescanAkyuz@users.noreply.github.com>
2024-04-17 16:24:48 +0300

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,28 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $to = "alperen.aydn16@hotmail.com"; // Alıcı e-posta adresi
+    $subject = "About survey assistance"; // E-posta konusu
+    $name = $_POST["name"]; // İsim alanından gelen veri
+    $email = $_POST["email"]; // E-posta alanından gelen veri
+    $phone = $_POST["phone"]; // Telefon alanından gelen veri
+    $message = $_POST["message"]; // Mesaj alanından gelen veri
+
+    // E-posta başlıkları
+    $headers = "From: $email\r\n" .
+        "Reply-To: $email\r\n" .
+        "X-Mailer: PHP/" . phpversion();
+
+    // E-posta içeriğini oluşturma
+    $email_content = "Gönderen İsim: $name\n" .
+        "Gönderen E-posta: $email\n" .
+        "Telefon: $phone\n\n" .
+        "Mesaj:\n$message";
+
+    // E-postayı oluşturup gönderme işlemi
+    if (mail($to, $subject, $email_content, $headers)) {
+        echo "E-posta başarıyla gönderildi.";
+    } else {
+        echo "E-posta gönderilirken bir hata oluştu.";
+    }
+}
+?>
